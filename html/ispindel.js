62617c0995924cac0035a42d3ee1640e1f9e0ae8
Radek Pilar <gitlab@mrkva.eu>
2022-04-11 21:25:20 +0100

    Added support for state persistence via hash URL

--- html/ispindel.js
+++ html/ispindel.js
@@ -12,6 +12,95 @@ var selectedColumnsStr = "date,temperature,gravity";
 var chartData = []
 var chart = {};
 
+function parseHash() {
+	if (!window.location.hash) {
+		return;
+	}
+
+
+	var hash = window.location.hash;
+
+	hash = decodeURI(hash.substring(1));
+
+	const hashObj = JSON.parse(hash);
+	console.log("parsing hash: "+hash);
+
+	if (hashObj.device) {
+		console.log("hash contains device: "+hashObj.device);
+		console.log("availableDevices: "+availableDevices);
+		for (const dev of availableDevices) {
+			console.log("available device: "+dev+", searching for: "+hashObj.device );
+			if (dev.name == hashObj.device) {
+				selectedDevice = dev;
+				document.getElementById("deviceSelect").value = dev.name;
+
+				updateDevice();
+				break;
+			}
+		}
+	}
+
+	if (hashObj.interval) {
+		console.log("hash contains interval: "+hashObj.interval);
+		selectedInterval = hashObj.interval;
+		updateInterval();
+	}
+
+	if (hashObj.columns) {
+		console.log("hash contains columns: "+hashObj.columns);
+		selectedColumns = hashObj.columns;
+		selectedColumnsStr = "";
+		for (const colElem of document.getElementsByName("column")) {
+			if (selectedColumns.includes(colElem.value)) {
+				colElem.checked = true;
+				selectedColumnsStr += colElem.value + ","
+			} else {
+				colElem.checked = false;
+			}
+		}
+		selectedColumnsStr = selectedColumnsStr.slice(0, -1);
+		columnUpdated();
+		reloadChart();
+	}
+
+}
+
+function generateHash() {
+	var hashObj = {};
+	hashObj.device = selectedDevice.name;
+	hashObj.interval = selectedInterval;
+	hashObj.columns = selectedColumns;
+
+	const hashStr = JSON.stringify(hashObj);
+	window.location.hash = hashStr;
+}
+
+
+function deviceSelected() {
+	var select = document.getElementById("deviceSelect");
+	var selectedName = select.options[select.selectedIndex].text;
+
+	for (const dev of availableDevices) {
+		if (dev.name == selectedName) {
+			selectedDevice = dev;
+			break;
+		}
+	}
+	selectedInterval = null;
+	updateDevice();
+	generateHash();
+
+}
+
+function intervalSelected() {
+	var select = document.getElementById("intervalSelect");
+	var selectedName = select.options[select.selectedIndex].value;
+	
+	selectedInterval = availableIntervals[selectedName];
+	updateInterval();
+	generateHash();
+}
+
 async function listDevices() {
 	await fetch("/cgi-bin/ispindel/listDevices.py")
 		.then(res => res.json())
@@ -50,7 +139,8 @@ async function listDevices() {
 
 	if (data.length == 1) {
 		document.getElementById("deviceSelect").value = lastDev;
-		deviceChosen();
+		selectedDevice = availableDevices[0];
+		updateDevice();
 	}
 }
 
@@ -84,25 +174,49 @@ async function listIntervals() {
 	str += "<option value=\"\">[Select interval]</option>";
 //	str += "<option value=\"-1\">[Everything]</option>";
 	var i = 0;
+
+	var foundSelected = undefined;
+
 	for (const intvl of data) {
+		var selected = "";
+		if (selectedInterval && selectedInterval.start == intvl.start && selectedInterval.end == intvl.end) {
+			foundSelected = i;
+		}
+		
 		str += "<option value=\"" + i + "\">" + (intvl.start != undefined ? intvl.start : "???") + " - " + (intvl.end != undefined ? intvl.end : "???")+ "</option>";
 		i += 1;
 	}
+	if (selectedInterval && foundSelected == undefined) {
+		str += "<option value=\"" + i + "\"d>" + (selectedInterval.start != undefined ? selectedInterval.start : "???") + " - " + (selectedInterval.end != undefined ? selectedInterval.end : "???")+ "</option>";
+		foundSelected = i;
+		i += 1;
+	}
+
 
 	document.getElementById("intervalSelect").innerHTML = str;
 	document.getElementById("intervalSelect").disabled = false;
-	document.getElementById("intervalSelect").value = i-1;
-	intervalChosen();
 
+	if (selectedInterval) {
+		document.getElementById("intervalSelect").value = foundSelected;
+	} else {
+		document.getElementById("intervalSelect").value = i-1;
+	}
+	
+	intervalSelected();
 }
 
 async function getChartData() {
+	if (!selectedDevice) {
+		console.log("Attempt to update chart data with no device selected!");
+		return;
+	}
 	var url = "/cgi-bin/ispindel/getData.py?device="+selectedDevice.name+"&columns="+selectedColumnsStr;
-	if (selectedInterval.start != undefined) {
+	
+	if (selectedInterval != undefined && selectedInterval.start != undefined) {
 		url += "&since="+selectedInterval.start;
 	}
 
-	if (selectedInterval.end != undefined) {
+	if (selectedInterval != undefined && selectedInterval.end != undefined) {
 		url += "&until="+selectedInterval.end;
 	}
 
@@ -224,25 +338,11 @@ async function getChartData() {
 
 
 
-function intervalChosen() {
-	var select = document.getElementById("intervalSelect");
-	var selectedName = select.options[select.selectedIndex].value;
-	
-	selectedInterval = availableIntervals[selectedName];
+function updateInterval() {
 	reloadChart();
 }
 
-function deviceChosen() {
-	var select = document.getElementById("deviceSelect");
-	var selectedName = select.options[select.selectedIndex].text;
-
-	for (const dev of availableDevices) {
-		if (dev.name == selectedName) {
-			selectedDevice = dev;
-			break;
-		}
-	}
-
+function updateDevice() {
 	var str = "";
 	
 	for (const col of selectedDevice.reported_data) {
@@ -273,13 +373,15 @@ function columnUpdated() {
 	}
 
 	reloadChart();
+	generateHash();
 }
 
 function reloadChart() {
 	getChartData();
 }
 
-window.onload = function() {
-	listDevices();
+window.onload = async function() {
+	await listDevices();
+	parseHash();
 }
 
